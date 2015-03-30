7f2ec805d63065a5e7e088d20c5a887089c3f3ee
altescape <hello@michaelwatts.me>
2015-03-30 13:28:42 +0100

    changed title to SMSW Slideshow

--- routes/slides.js
+++ routes/slides.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET slide 1. */
 router.get('/slides', function(req, res, next) {
-  res.render('slides', { title: 'Express' });
+  res.render('slides', { title: 'SMSW Slideshow' });
 });
 
 module.exports = router;
